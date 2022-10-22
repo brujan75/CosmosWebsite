7aac3ea6ab4fa789cb2f59596cb8f8bd6f31691f
brujan75 <andreibrujique@yahoo.com>
2022-10-22 12:06:15 +0300

    added to repo

---
+++ scrips/script.js
@@ -0,0 +1,39 @@
+ 
+const navSlide = () => {
+  const burger = document.querySelector(".burger");
+  const nav = document.querySelector(".navbar");
+  burger.addEventListener("click", () => {
+    nav.classList.toggle("active"); 
+  });
+};
+navSlide();
+
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+ 
